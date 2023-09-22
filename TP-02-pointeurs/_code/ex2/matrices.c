d0f6990a8d14e9a798c4442115bb2f5d2c54f7af
eliot <eliot.shaw@etu.imt-nord-europe.fr>
2023-09-22 10:51:56 +0200

    matrices fonctionnelles

--- TP-02-pointeurs/_code/ex2/matrices.c
+++ TP-02-pointeurs/_code/ex2/matrices.c
@@ -6,15 +6,21 @@ int main(void) {
     int64_t matrice2[][SIZE]={{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10}};
     int64_t matriceResultat[SIZE][SIZE];
     matrix_mult(matriceResultat,matrice1,matrice2);
-    matrix_print(matriceResultat);
+    matrix_printSize(matriceResultat);
+
+    Matrice mat33 = matrix_creer(8, 3, 2);
+    matrix_print(mat33);
+
+
     return EXIT_SUCCESS;
+
 }
 
 
 void matrix_mult(int64_t matriceResultat[][SIZE], int64_t matrice1[][SIZE], int64_t matrice2[][SIZE]){
     for(int lig=0; lig<SIZE; lig++){
         for(int col=0; col<SIZE; col++){
-            matriceResultat[lig][col]=matrix_mult_locale(matrice1,matrice2, lig, col);
+            matriceResultat[lig][col]=matrix_mult_locale(matrice1, matrice2, lig, col);
         }
     }
 }
@@ -29,7 +35,7 @@ int matrix_mult_locale(int64_t matrice1[][SIZE], int64_t matrice2[][SIZE], int l
 }
 
 
-void matrix_print(int64_t matriceResultat[][SIZE]){
+void matrix_printSize(int64_t matriceResultat[][SIZE]){
     printf("\nImpression de la matrice finale :\n\n\n");
     for(int lig=0; lig<SIZE; lig++){
         for(int col=0; col<SIZE; col++){
@@ -42,12 +48,17 @@ void matrix_print(int64_t matriceResultat[][SIZE]){
 }
 
 
-Matrice creer(int64_t valInit, int64_t nbLig, int64_t nbCol){
+Matrice matrix_creer(int64_t valInit, int64_t nbLig, int64_t nbCol){
     Matrice nouvelleMatrice;
     nouvelleMatrice.nbCol = nbCol;
     nouvelleMatrice.nbLig = nbLig;
     nouvelleMatrice.mat = malloc(sizeof(int64_t)*nbCol*nbLig);
 
+    for (int r=0; r < nouvelleMatrice.nbLig; ++r){
+        int64_t* row = matrix_lig(nouvelleMatrice, r);
+        for (int c=0; c < nouvelleMatrice.nbCol; ++c)
+            row[c] = valInit;
+    }
     return nouvelleMatrice;
 }
 
@@ -55,7 +66,19 @@ void matrix_destroy(struct Matrice* matADestroy){
     free(matADestroy->mat);
 }
 
-int64_t* matrix_lig(Matrice matriceAObs,int64_t nbLig){
+int64_t* matrix_lig(Matrice matriceAObs, int64_t nbLig){
     return (matriceAObs.mat + (nbLig * matriceAObs.nbCol));
 }
 
+void matrix_print(Matrice matriceResultat){
+    printf("\nImpression de la matrice finale :\n\n\n");
+    for(int lig=0; lig<matriceResultat.nbLig; lig++){
+        for(int col=0; col<matriceResultat.nbCol; col++){
+            int64_t* row = matrix_lig(matriceResultat, lig);
+            printf("%d", row[col]);
+            printf("\t");
+        }
+        printf("\n");
+        printf("\n");
+    }
+}
--- TP-02-pointeurs/_code/ex2/matrices.h
+++ TP-02-pointeurs/_code/ex2/matrices.h
@@ -14,7 +14,8 @@ typedef struct Matrice{
 
 void matrix_mult(int64_t matriceResultat[][SIZE], int64_t matrice1[][SIZE], int64_t matrice2[][SIZE]);
 int matrix_mult_locale(int64_t matrice1[][SIZE], int64_t matrice2[][SIZE], int lig, int col);
-void matrix_print(int64_t matriceResultat[][SIZE]);
-Matrice creer(int64_t valInit, int64_t nbLig, int64_t nbCol);
+void matrix_printSize(int64_t matriceResultat[][SIZE]);
+Matrice matrix_creer(int64_t valInit, int64_t nbLig, int64_t nbCol);
 void matrix_destroy(struct Matrice* matADestroy);
-int64_t* matrix_Lig(Matrice matrice,int64_t nbLig);
+int64_t* matrix_lig(Matrice matrice,int64_t nbLig);
+void matrix_print(Matrice matriceResultat);
